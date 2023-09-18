cc0fa254575bcf485f96ae2fc5d6b4909adc0a89
Joe D'Amore <joe@joedamore.me>
2023-09-17 21:33:42 -0400

    Add initial config

---
+++ configuration.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+{
+  imports = 
+    [
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  # Networking.
+  networking.hostName = "nixos";
+  # networking.wireless.enable = true;
+  networking.networkmanager.enable = true;
+
+  # Time zone and locality.
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.utf8";
+
+  # X11 keymap.
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # User accounts.
+  users.users.joe = {
+    isNormalUser = true;
+    description = "Joe D'Amore";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages.
+  nixpkgs.config.allowUnfree = true;
+
+  # System packages.
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+    curl
+  ];
+
+  # OpenSSH.
+  services.openssh.enable = true;
+
+  # Do not edit.
+  system.stateVersion = "22.05";
+}
