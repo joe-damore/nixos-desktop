2e4f7b2fda329a965c23bdcabbcac20ccdac08fb
Joe D'Amore <joe@joedamore.me>
2023-12-08 12:25:06 -0500

    Attempt to configure cursor

--- home/joe/home.nix
+++ home/joe/home.nix
@@ -5,6 +5,32 @@
   home.homeDirectory = "/home/joe";
 
   # ------------------ #
+  # GTK configuration. #
+  # ------------------ #
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Amber";
+    size = 32;
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+    iconTheme = {
+      package = pkgs.libsForQt5.breeze-icons;
+      name = "breeze-dark";
+    };
+    font = {
+      name = "Sans";
+      size = 12;
+    };
+  };
+
+  # ------------------ #
   # ZSH configuration. #
   # ------------------ #
   programs.zsh = {
