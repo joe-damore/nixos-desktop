501bc3f1c849a6fd946a061c027f5146372169a0
Joe D'Amore <joe@joedamore.me>
2023-12-08 08:55:01 -0500

    Install zip, kitty, and Firefox

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -2,11 +2,26 @@
 
 { config, pkgs, ... }:
 {
+  # Basic system packages.
   environment.systemPackages = with pkgs; [
     vim
     wget
     git
     curl
     neofetch
+    zip
+    kitty
   ];
+
+  # Neovim.
+  programs.neovim = {
+    enable = true;
+    defaultEditor = false;
+  };
+
+  # Firefox.
+  programs.firefox = {
+    enable = true;
+    package = pkgs.latest.firefox-bin;
+  };
 }
