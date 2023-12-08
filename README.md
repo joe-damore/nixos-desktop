aac25e75fd66d39fa1a42e49fe81f47f563b6beb
Joe D'Amore <joe@joedamore.me>
2023-12-07 19:37:45 -0500

    Add Framework 13 configuration

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nixos-desktop
 Experimental NixOS Configuration for Desktop PC
+
+## Rebuild Command
+
+```bash
+sudo nixos-rebuild switch --flake '/etc/nixos#framework-13'
+```
--- flake.nix
+++ flake.nix
@@ -2,20 +2,27 @@
   description = "Joe's NixOS Desktop";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations = {
-      "dell-inspiron-15" = nixpkgs.lib.nixosSystem {
+      "framework-13" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
-          ./hosts/dell-inspiron-15/configuration.nix
+          ./hosts/framework-13/configuration.nix
         ];
       };
+      # "dell-inspiron-15" = nixpkgs.lib.nixosSystem {
+      #   system = "x86_64-linux";
+      #   specialArgs = { inherit inputs; };
+      #   modules = [
+      #     ./hosts/dell-inspiron-15/configuration.nix
+      #   ];
+      # };
     };
   };
 
