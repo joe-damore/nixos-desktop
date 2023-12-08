47a53bfd6fcc6b9f5550261e4e77d6d1c010b048
Joe D'Amore <joe@joedamore.me>
2023-12-08 08:26:26 -0500

    Enable binary cache

--- hosts/framework-13/configuration.nix
+++ hosts/framework-13/configuration.nix
@@ -24,6 +24,16 @@
     NIXOS_REBUILD_PATH = "/etc/nixos";
   };
 
+  settings = {
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
