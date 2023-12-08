32d5311addbc425246861465750956f4882f2422
Joe D'Amore <joe@joedamore.me>
2023-12-08 18:15:51 -0500

    Enable XDG

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -1,4 +1,15 @@
 {inputs, pkgs, ...}: {
+  xdg = {
+    autostart.enable = true;
+    portal = {
+      enable = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal
+        pkgs.xdg-desktop-portal-gtk
+      ];
+    };
+  };
+
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
