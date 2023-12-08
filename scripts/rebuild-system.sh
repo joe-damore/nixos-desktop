649f214c978dd78e952320ec72fd5abf69b90f5b
Joe D'Amore <joe@joedamore.me>
2023-12-07 23:20:14 -0500

    Add rebuild script

--- scripts/rebuild-system.sh
+++ scripts/rebuild-system.sh
@@ -1,3 +1,9 @@
-echo "Hello, world!"
-echo "$NIXOS_REBUILD_CONFIG"
-echo "$NIXOS_REBUILD_PATH"
+# Exit if not running as root.
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+
+cd "$NIXOS_REBUILD_PATH"
+git pull
+nixos-rebuild switch --flake "$NIXOS_REBUILD_PATH#$NIXOS_REBUILD_CONFIG"
