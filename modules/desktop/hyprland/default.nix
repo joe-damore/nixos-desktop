f032fd643fbf485ce536a348bc2e327d5e5acbc5
Joe D'Amore <joe@joedamore.me>
2023-12-08 19:28:37 -0500

    Remnove unneeded xdg config

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -1,14 +1,13 @@
 {inputs, pkgs, ...}: {
-  xdg = {
-    autostart.enable = true;
-    portal = {
-      enable = true;
-      extraPortals = [
-        pkgs.xdg-desktop-portal
-        pkgs.xdg-desktop-portal-gtk
-      ];
-    };
-  };
+  # xdg = {
+  #   autostart.enable = true;
+  #   portal = {
+  #     enable = true;
+  #     extraPortals = [
+  #       pkgs.xdg-desktop-portal-gtk
+  #     ];
+  #   };
+  # };
 
   programs.hyprland = {
     enable = true;
